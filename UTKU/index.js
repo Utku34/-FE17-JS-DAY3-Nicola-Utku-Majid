706b9640a2a6985a7adb7be92b668d968e6f6e68
Utku34 <utku.tarik.ozkaya@gmail.com>
2022-09-28 21:52:40 +0200

    added changes 2

---
+++ UTKU/index.js
@@ -0,0 +1,17 @@
+let car = {
+    fuel: 90,
+    brand: 'Mercedes',
+    model: 'C 63 ///AMG',
+}
+
+let animal = {
+    ageToLive: 18,
+    animalType: 'Cat',
+    food: 'fish',
+}
+
+let person = {
+    age: 90,
+    Vorname: 'Utku',
+    Nachname: 'Ozkaya',
+}
